69c7262eaa3b7b8ac2556d2920bea28c661cbeb0
Achiles Santos <achillesgomess@gmail.com>
2025-01-24 00:29:55 -0300

    improve lint validate for utils/core.py and yours tests;

--- tests/unit/utils/test_core.py
+++ tests/unit/utils/test_core.py
@@ -6,8 +6,7 @@ from cherry_tree.utils.core import get_screenshot, locate, read_image
 
 @pytest.fixture
 def mock_camera(mocker):
-    mock_camera = mocker.patch("cherry_tree.utils.core.camera")
-    return mock_camera
+    return mocker.patch("cherry_tree.utils.core.camera")
 
 
 @pytest.fixture
@@ -35,56 +34,58 @@ def mock_os_path_exists(mocker):
     return mocker.patch("os.path.exists")
 
 
-def test_get_screenshot_success(mock_camera, mock_cv2_cvt_color):
+def test_get_screenshot_success(mock_camera_fixture, mock_cv2_cvt_color_fixture):
     # Configura o mock para retornar uma imagem simulada
     fake_image = MagicMock()
-    mock_camera.grab.return_value = fake_image
+    mock_camera_fixture.grab.return_value = fake_image
     result = get_screenshot()
 
     assert result is not None
 
-    mock_camera.grab.assert_called_once()
-    mock_cv2_cvt_color.assert_called_once()
+    mock_camera_fixture.grab.assert_called_once()
+    mock_cv2_cvt_color_fixture.assert_called_once()
 
 
-def test_get_screenshot_no_image(mocker, mock_camera, mock_cv2_cvt_color):
-    mock_camera.grab.return_value = None
+def test_get_screenshot_no_image(
+    mocker, mock_camera_fixture, mock_cv2_cvt_color_fixture
+):
+    mock_camera_fixture.grab.return_value = None
 
     # Define um valor inicial para `latest_screenshot`
-    latest_screenshot = mock_cv2_cvt_color
+    latest_screenshot = mock_cv2_cvt_color_fixture
     mocker.patch("cherry_tree.utils.core.latest_screenshot", latest_screenshot)
 
     result = get_screenshot()
 
     # Verifica se o resultado é o último valor conhecido
     assert result == latest_screenshot
-    mock_camera.grab.assert_called_once()
+    mock_camera_fixture.grab.assert_called_once()
 
 
-def test_get_screenshot_updates_latest(mocker, mock_camera):
+def test_get_screenshot_updates_latest(mocker, mock_camera_fixture):
     # Configura o mock para retornar uma imagem simulada
     fake_image = MagicMock()
-    mock_camera.grab.return_value = fake_image
+    mock_camera_fixture.grab.return_value = fake_image
 
     # Mocka cv2.cvtColor
-    mock_cv2_cvt_color = mocker.patch("cv2.cvtColor", return_value="gray_image")
+    cv2_cvt_color = mocker.patch("cv2.cvtColor", return_value="gray_image")
     latest_screenshot = mock_cv2_cvt_color.return_value
 
     # Testa a função
     result = get_screenshot()
 
     # Verifica se `cv2.cvtColor` foi chamado corretamente
-    mock_cv2_cvt_color.assert_called_once_with(fake_image, cv2.COLOR_BGRA2GRAY)
+    cv2_cvt_color.assert_called_once_with(fake_image, cv2.COLOR_BGRA2GRAY)
 
     # Verifica se o resultado e o `latest_screenshot` foram atualizados
     assert result == "gray_image"
     assert latest_screenshot == "gray_image"
 
 
-def test_locate_success(mock_cv2_min_max_loc, mock_cv2_match_template):
+def test_locate_success(mock_cv2_min_max_loc_fixture, mock_cv2_match_template_fixture):
     # Simula os retornos do matchTemplate e minMaxLoc
-    mock_cv2_match_template.return_value = MagicMock()
-    mock_cv2_min_max_loc.return_value = (None, 0.9, None, (50, 50))
+    mock_cv2_match_template_fixture.return_value = MagicMock()
+    mock_cv2_min_max_loc_fixture.return_value = (None, 0.9, None, (50, 50))
 
     screenshot = MagicMock()
     template = MagicMock()
@@ -93,14 +94,16 @@ def test_locate_success(mock_cv2_min_max_loc, mock_cv2_match_template):
     result = locate(screenshot, template, confidence=0.85)
 
     assert result == (65, 60, 30, 20)
-    mock_cv2_match_template.assert_called_once()
-    mock_cv2_min_max_loc.assert_called_once()
+    mock_cv2_match_template_fixture.assert_called_once()
+    mock_cv2_min_max_loc_fixture.assert_called_once()
 
 
-def test_locate_low_confidence(mock_cv2_min_max_loc, mock_cv2_match_template):
+def test_locate_low_confidence(
+    mock_cv2_min_max_loc_fixture, mock_cv2_match_template_fixture
+):
     # Configura uma confiança baixa
-    mock_cv2_match_template.return_value = MagicMock()
-    mock_cv2_min_max_loc.return_value = (None, 0.7, None, (50, 50))
+    mock_cv2_match_template_fixture.return_value = MagicMock()
+    mock_cv2_min_max_loc_fixture.return_value = (None, 0.7, None, (50, 50))
 
     screenshot = MagicMock()
     template = MagicMock()
@@ -108,8 +111,8 @@ def test_locate_low_confidence(mock_cv2_min_max_loc, mock_cv2_match_template):
     with pytest.raises(ValueError, match="Confidence matching was below the threshold"):
         locate(screenshot, template, confidence=0.85)
 
-    mock_cv2_match_template.assert_called_once()
-    mock_cv2_min_max_loc.assert_called_once()
+    mock_cv2_match_template_fixture.assert_called_once()
+    mock_cv2_min_max_loc_fixture.assert_called_once()
 
 
 def test_locate_invalid_inputs():
@@ -123,31 +126,31 @@ def test_locate_invalid_inputs():
         locate(MagicMock(), template)
 
 
-def test_read_image_success(mocker, mock_os_path_exists):
-    mock_os_path_exists.return_value = True
+def test_read_image_success(mocker, mock_os_path_exists_fixture):
+    mock_os_path_exists_fixture.return_value = True
 
     # Simula a imagem retornada por imread
     mocker.patch("cv2.imread", return_value=MagicMock())
     result = read_image("valid_path.jpg")
     assert result is not None
-    mock_os_path_exists.assert_called_once_with("valid_path.jpg")
+    mock_os_path_exists_fixture.assert_called_once_with("valid_path.jpg")
 
 
-def test_read_image_file_not_found(mock_os_path_exists):
-    mock_os_path_exists.return_value = False
+def test_read_image_file_not_found(mock_os_path_exists_fixture):
+    mock_os_path_exists_fixture.return_value = False
 
     with pytest.raises(FileNotFoundError, match="Image file not found"):
         read_image("invalid_path.jpg")
 
-    mock_os_path_exists.assert_called_once_with("invalid_path.jpg")
+    mock_os_path_exists_fixture.assert_called_once_with("invalid_path.jpg")
 
 
-def test_read_image_load_failure(mocker, mock_os_path_exists):
-    mock_os_path_exists.return_value = True
+def test_read_image_load_failure(mocker, mock_os_path_exists_fixture):
+    mock_os_path_exists_fixture.return_value = True
 
     # Simula falha no carregamento da imagem
     mocker.patch("cv2.imread", return_value=None)
     with pytest.raises(ValueError, match="Failed to load image at path"):
         read_image("invalid_image.jpg")
 
-    mock_os_path_exists.assert_called_once_with("invalid_image.jpg")
+    mock_os_path_exists_fixture.assert_called_once_with("invalid_image.jpg")
